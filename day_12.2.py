839a992d58076bcd030f15080278691abfdfdc97
w-ema <wema314@gmail.com>
2023-07-29 00:41:59 +0200

    solutions for 12th day

---
+++ day_12.2.py
@@ -0,0 +1,45 @@
+# Dijkstra algorithm
+
+from collections import deque
+with open(r'C:\Users\cp\Desktop\AoC-22\inputs\input_12.txt', 'r') as f:
+    inp = f.read()
+
+map = []
+for i in inp.splitlines():
+    row = []
+    for j in i:
+        row.append(j)
+    map.append(row)
+
+dict = {}
+
+for r in range(len(map)):
+    for c in range(len(map[0])):
+        if map[r][c] == 'E':
+            end = [r,c]
+            map[r][c] = 'z'
+        elif map[r][c] == 'S':
+            start = [r,c]
+            map[r][c] = 'a'
+        neighbours = []
+        for i in [[r-1,c],[r+1,c],[r,c+1],[r,c-1]]:
+            if i[0] in range(len(map)) and i[1] in range(len(map[0])):
+                if ord(map[i[0]][i[1]]) - ord(map[r][c]) >= -1:
+                    neighbours.append(i)
+        dict[str([r,c])] = neighbours
+
+q = deque()
+q.append((0, end))
+
+vis = [end]
+
+while q:
+    d, node = q.popleft()
+    for n_node in dict[str(node)]:
+        if map[n_node[0]][n_node[1]] == 'a':
+            print(d+1)
+            quit()
+        if n_node in vis:
+            continue
+        vis.append(n_node)
+        q.append((d+1,n_node))
